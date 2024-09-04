ac52563a542144452119b87bf7d4c76198ad99ef
Ilham311 <ilham@temp-mails.eu.org>
2024-09-05 03:30:56 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,81 +1,166 @@
-from telethon import TelegramClient, events
-import base64
-import requests
+import os
+try:
+    import requests, colorama, prettytable, webbrowser
+except:
+    os.system("pip install requests")
+    os.system("pip install colorama")
+    os.system("pip install prettytable")
+    os.system("pip install webbrowser")
+import threading, requests, ctypes, random, json, time, base64, sys, re
+from prettytable import PrettyTable
+import random
+from time import strftime
+import webbrowser
+from colorama import init, Fore
+from urllib.parse import urlparse, unquote, quote
+from string import ascii_letters, digits
 
-API_ID = '961780'
-API_HASH = 'bbbfa43f067e1e8e2fb41f334d32a6a7'
-BOT_TOKEN = '7375007973:AAEDZnqXwCGmJ-fmCkT0PuHzdRLFYsKcIAg'
-
-client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-
-def get_spotify_track_recommendations(keyword, search_type='track', limit=6):
-    SPOTIFY_CLIENT_ID = 'd0c345f7c8f7437f9001583b3bdae340'
-    SPOTIFY_CLIENT_SECRET = '18cf1155bba744dda945b70325ca71cc'
-    client_credentials = f'{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}'
-    base64_credentials = base64.b64encode(client_credentials.encode('utf-8')).decode('utf-8')
-    headers = {'Authorization': f'Basic {base64_credentials}'}
-    token_endpoint = 'https://accounts.spotify.com/api/token'
-
-    token_response = requests.post(token_endpoint, headers=headers, data={'grant_type': 'client_credentials'})
-    if token_response.status_code == 200:
-        access_token = token_response.json().get('access_token')
-        headers['Authorization'] = f'Bearer {access_token}'
-        search_endpoint = 'https://api.spotify.com/v1/search'
-        response = requests.get(search_endpoint, params={'q': keyword, 'type': search_type, 'limit': limit}, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        return {'error': f"Search Error: {response.status_code} - {response.text}"}
-    return {'error': f"Token Error: {token_response.status_code} - {token_response.text}"}
-
-def download_spotify_track(track_id, track_name, artist_name=None):
-    download_url = f'https://yank.g3v.co.uk/track/{track_id}'
-    response = requests.get(download_url)
-    if response.status_code == 200:
-        filename = f"{track_name} by {artist_name}.mp3" if artist_name else f"{track_name} ({track_id}).mp3"
-        with open(filename, 'wb') as file:
-            file.write(response.content)
-        return filename
-    return None
-
-@client.on(events.NewMessage(pattern='/lagu (.+)'))
-async def handle_song_request(event):
-    keyword = event.pattern_match.group(1)
-    message = await event.respond(f"🔍 Mencari Musik '{keyword}'...")
-
-    recommendations = get_spotify_track_recommendations(keyword)
-    if 'error' in recommendations:
-        await message.edit(recommendations['error'])
-        return
-
-    tracks = recommendations['tracks']['items']
-    if not tracks:
-        await message.edit(f"No tracks found for '{keyword}'.")
-        return
-
-    first_track = tracks[0]
-    track_name = first_track['name']
-    artist_name = ', '.join(artist['name'] for artist in first_track['artists']) if first_track['artists'] else None
-    track_id = first_track['id']
-
-    await message.edit(f"⬇️ Downloading: {track_name} by {artist_name if artist_name else 'Unknown Artist'}")
-    filename = download_spotify_track(track_id, track_name, artist_name)
+class Zefoy:
     
-    if filename:
-        await message.edit(f"📤 Uploading: {track_name} by {artist_name if artist_name else 'Unknown Artist'}")
-        await client.send_file(event.chat_id, filename)
+    def __init__(self):
+        self.base_url = 'https://zefoy.com/'
+        self.headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
+        self.session = requests.Session()
+        self.captcha_1 = None
+        self.captcha_ = {}
+        self.service = 'Views'
+        self.video_key = None
+        self.services = {}
+        self.services_ids = {}
+        self.services_status = {}
+        ################################
+        self.url = 'https://vt.tiktok.com/ZS2UQa7q2/'
+        ################################
+        ################################
+        self.text = 'VIEWTIKTOK'
         
-        user = await client.get_entity(event.sender_id)
-        username = user.username if user.username else 'Unknown User'
+    def get_captcha(self):
+        if os.path.exists('session'): self.session.cookies.set("PHPSESSID", open('session',encoding='utf-8').read(), domain='zefoy.com')
+        request = self.session.get(self.base_url, headers=self.headers)
+        if 'Enter Video URL' in request.text: self.video_key = request.text.split('" placeholder="Enter Video URL"')[0].split('name="')[-1]; return True
 
-        music_info = (
-            f"➲ 𝙽𝙰𝙼𝙴: {track_name} {'by ' + artist_name if artist_name else ''}\n"
-            f"➲ 𝙰𝚁𝚃𝙸𝚂𝚃: {artist_name if artist_name else 'Unknown Artist'}\n"
-            f"➲ 𝙳𝚄𝚁𝙰𝚃𝙸𝙾𝙽: Unknown\n"
-            f"➲ 𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝙳 𝙱𝚈: @{username} ⚡"
-        )
-        await client.send_message(event.chat_id, music_info)
-    else:
-        await message.edit(f"Failed to download track '{track_name}'.")
+        try:
+            for x in re.findall(r'<input type="hidden" name="(.*)" value="(.*)">', request.text): self.captcha_[x[0]] = x[1]
 
-client.start()
-client.run_until_disconnected()
+            self.captcha_1 = request.text.split('type="text" name="')[1].split('" oninput="this.value=this.value.toLowerCase()"')[0]
+            captcha_url = request.text.split('<img src="')[1].split('" onerror="imgOnError()" class="')[0]
+            request = self.session.get(f"{self.base_url}{captcha_url}",headers=self.headers)
+            open('captcha.png', 'wb').write(request.content)
+            print('Captcha solving....')
+            return False
+        except Exception as e:
+            time.sleep(2)
+            self.get_captcha()
+
+    def get_status_services(self):
+        request = self.session.get(self.base_url, headers=self.headers).text
+        for x in re.findall(r'<h5 class="card-title">.+</h5>\n.+\n.+', request): self.services[x.split('<h5 class="card-title">')[1].split('<')[0].strip()] = x.split('d-sm-inline-block">')[1].split('</small>')[0].strip()
+        for x in re.findall(r'<h5 class="card-title mb-3">.+</h5>\n<form action=".+">', request): self.services_ids[x.split('title mb-3">')[1].split('<')[0].strip()] = x.split('<form action="')[1].split('">')[0].strip()
+        for x in re.findall(r'<h5 class="card-title">.+</h5>\n.+<button .+', request): self.services_status[x.split('<h5 class="card-title">')[1].split('<')[0].strip()] = False if 'disabled class' in x else True
+        return (self.services, self.services_status)
+
+    def run(self):
+        self.send_captcha()
+        while True:
+            try:
+                if 'Service is currently not working, try again later' in str(self.use_service()):
+                    print(f'The service is currently unavailable, please try again later.')
+                    time.sleep(5)
+            except Exception as e:
+                print(f'ERROR | try again after 10 seconds.|| {e}')
+                time.sleep(10)
+
+    def send_captcha(self, new_session = False):
+        if new_session: self.session = requests.Session(); os.remove('session'); time.sleep(2)
+        if self.get_captcha():
+            return (True, 'The session already exists')
+        captcha_solve = self.solve_captcha('captcha.png')[1]
+        self.captcha_[self.captcha_1] = captcha_solve
+        request = self.session.post(self.base_url, headers=self.headers, data=self.captcha_)
+
+        if 'Enter Video URL' in request.text: 
+            open('session','w',encoding='utf-8').write(self.session.cookies.get('PHPSESSID'))
+            self.video_key = request.text.split('" placeholder="Enter Video URL"')[0].split('name="')[-1]
+            return (True,captcha_solve)
+        else: return (False,captcha_solve)
+
+    def solve_captcha(self, path_to_file = None, b64 = None, delete_tag = ['\n','\r']):
+        if path_to_file: task = path_to_file
+        else: open('temp.png','wb').write(base64.b64decode(b64)); task = 'temp.png'
+        request = self.session.post('https://api.ocr.space/parse/image?K87899142388957', headers={'apikey':'K87899142388957'}, files={'task':open(task,'rb')}).json()
+        solved_text = request['ParsedResults'][0]['ParsedText']
+        for x in delete_tag: solved_text = solved_text.replace(x,'')
+        return (True, solved_text)
+
+    def get_table(self, i = 1):
+        table = PrettyTable(field_names=["ID", "Update By ", "Status"], title="Status Services", header_style="upper",border=True)
+        while True:
+            if len(self.get_status_services()[0])>1:break
+            else:
+                exit("bad net")
+                self.send_captcha()
+                time.sleep(2)
+
+    def find_video(self):
+        if self.service is None: return (False, "You didn't choose the service")
+        while True:
+            if self.service not in self.services_ids: self.get_status_services(); time.sleep(1)
+            request = self.session.post(f'{self.base_url}{self.services_ids[self.service]}', headers={'content-type':'multipart/form-data; boundary=----WebKitFormBoundary0nU8PjANC8BhQgjZ', 'user-agent':self.headers['user-agent'], 'origin':'https://zefoy.com'}, data=f'------WebKitFormBoundary0nU8PjANC8BhQgjZ\r\nContent-Disposition: form-data; name="{self.video_key}"\r\n\r\n{self.url}\r\n------WebKitFormBoundary0nU8PjANC8BhQgjZ--\r\n')
+            try: self.video_info = base64.b64decode(unquote(request.text.encode()[::-1])).decode()
+            except: time.sleep(3); continue
+            if 'Session expired re-login' in self.video_info:
+                self.send_captcha()
+                return
+            elif 'service not working' in self.video_info:
+                return (True, 'The service is currently unavailable, please try again later.')
+            elif """onsubmit="showHideElements""" in self.video_info:
+                self.video_info = [self.video_info.split('" name="')[1].split('"')[0],self.video_info.split('value="')[1].split('"')[0]]
+                return (True, request.text)
+            elif 'Checking Timer...' in self.video_info:
+                try: 
+                    t=int(re.findall(r'ltm=(\d*);', self.video_info)[0])
+                    zyfoy = int(re.findall(r'ltm=(\d*);', self.video_info)[0])
+                except: 
+                    return (False,)
+                if zyfoy==0:self.find_video()
+                elif zyfoy >= 1000:
+                    print('IP BLOCKED')
+                _=time.time()
+                while time.time()-2<_+zyfoy:
+                    t-=1
+                    print(" - \033[1;31mWait:\033[1;36m {0} ".format(t)+"\033[1;35msecond\033[1;32m .", end="\r")
+                    
+                    time.sleep(1)
+                    
+                continue
+                    
+            elif 'Terlalu Banyak Permintaan' in self.video_info:
+                time.sleep(3)
+            else:
+                print(self.video_info)
+
+    def use_service(self):
+        if self.find_video()[0] is False:
+            return False
+        self.token = "".join(random.choices(ascii_letters+digits, k=16))
+        request = self.session.post(f'{self.base_url}{self.services_ids[self.service]}', headers={'content-type':f'multipart/form-data; boundary=----WebKitFormBoundary{self.token}', 'user-agent':self.headers['user-agent'], 'origin':'https://zefoy.com'}, data=f'------WebKitFormBoundary{self.token}\r\nContent-Disposition: form-data; name="{self.video_info[0]}"\r\n\r\n{self.video_info[1]}\r\n------WebKitFormBoundary{self.token}--\r\n')
+        try:
+            res = base64.b64decode(unquote(request.text.encode()[::-1])).decode()
+        except:
+            time.sleep(3)
+            return ""
+        if 'Session expired re-login' in res:
+            print('Session expired. Logging in again...')
+            self.send_captcha()
+            return ""
+        elif 'Too many requests. Please slow' in res:
+            time.sleep(3)
+        elif 'service not working' in res:
+            return ('The service is currently unavailable, please try again later.')
+        else:
+            print(res.split("sans-serif;text-align:center;color:green;'>")[1].split("</")[0])
+
+
+if __name__ == "__main__":
+    Z = Zefoy()
+    threading.Thread(target=Z.run).start()
